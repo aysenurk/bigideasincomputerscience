041fa17a40a9f593f73328bad0e19dc735493194
omerhayat <engineeromerhayat@gmail.com>
2017-08-02 10:16:05 +0200

    Add files via upload

--- fitnessfunction.py
+++ fitnessfunction.py
@@ -1,30 +1,31 @@
 import copy
-
 currentPositionCoordinate=[3,2]
-validationList=[]
 n = 4
-mylist=["UR","DR","UL"]
-def calculateLengthOfPossibleMoves( list ):
-   newList = copy.copy(currentPositionCoordinate)
+mylist=["UR","DR","UL","DR"]
+validationList = []
+
+
+def calculateLengthOfPossibleMoves(moveList, start):
+   global validationList
+   validationList = []
+   newList = list(start)
    addMoveToVisitedBlocksList(newList)
-   for move in list:
-       if(isValidMove(move)): #to check if the move is a valid knight move on chess board
-           newList = copy.copy(currentPositionCoordinate)
+   for move in moveList:
+       if(isValidMove(move, newList)): #to check if the move is a valid knight move on chess board
            if checkIfBlockIsNotAlreadyVisited(newList):  #check if the block is not already been visited by a knight first
                addMoveToVisitedBlocksList(newList) #add move to visited blocks list so you can keep a record not to visit it again
-   return validationList.__len__()-1 #return one less of a total is beacause starting position is also kept in validation list so we donot visit it again
+           else:
+               break
+   return len(validationList)-1 #return one less of a total is beacause starting position is also kept in validation list so we donot visit it again
 
 
-def isValidMove(move):
-    checkIfMoveIsValid(move)
-    if checkIfMoveDoesTakeUsOutOfTheChessBoard(currentPositionCoordinate):
-        return True
-    else:
-        return False
+def isValidMove(move, newList):
+    checkIfMoveIsValid(move, newList)
+    return checkIfMoveDoesTakeUsOutOfTheChessBoard(newList)
 
 def checkIfMoveDoesTakeUsOutOfTheChessBoard(currentPositionCoordinate):
     return (currentPositionCoordinate[0]>0  and currentPositionCoordinate[0]<=n and currentPositionCoordinate[1]>0 and currentPositionCoordinate[1]<=n)
-def checkIfMoveIsValid(move):
+def checkIfMoveIsValid(move, currentPositionCoordinate):
     if (move == "UR"):
         currentPositionCoordinate[0] = currentPositionCoordinate[0] - 2
         currentPositionCoordinate[1] = currentPositionCoordinate[1] + 1
@@ -58,7 +59,8 @@ def checkIfBlockIsNotAlreadyVisited(currentPositionCoordinate):
         return True
 
 def addMoveToVisitedBlocksList(move):
-    validationList.append(move)
+    validationList.append(list(move))
 
 
-print (calculateLengthOfPossibleMoves(mylist))
+print (calculateLengthOfPossibleMoves(mylist, [3,2]))
+print (calculateLengthOfPossibleMoves(mylist, [3,2]))
